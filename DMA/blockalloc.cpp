5beead4cec307b62ecead3047a2db3d4dddacbac
Aryan Salaria <arayn.32@gmail.com>
2023-10-31 00:15:52 +0530

    Dynamic Memory Allocation For Arrays

---
+++ DMA/blockalloc.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int *ptr = NULL;
+
+    ptr = new int(n);
+
+    for(int i=0 ; i<n ; i++)
+    {
+        cin >> ptr[i];
+    }
+
+    for(int i=0 ; i<n ; i++)
+    {
+        cout << ptr[i] << " ";
+    }
+
+    cout << endl;
+
+    delete []ptr;
+}
